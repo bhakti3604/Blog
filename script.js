9ddc2bcdd572ea1d7ac4d5b54460413dc67e0577
Bhakti <bhaktishyara3604@gmail.com>
2025-04-28 16:03:01 +0530

    [UPDATE] Like feature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Sample data for blog posts
 const posts = [
-  { title: "My First Blog Post", content: "This is the content of my first blog post." },
-  { title: "Another Post", content: "This is another blog post with more content." }
+  { title: "My First Blog Post", content: "This is the content of my first blog post.", likes: 0 },
+  { title: "Another Post", content: "This is another blog post with more content.", likes: 0 }
 ];
 
 // Function to render posts to the page
@@ -9,7 +9,7 @@ function renderPosts() {
   const postContainer = document.getElementById("post-container");
   postContainer.innerHTML = ''; // Clear existing posts
 
-  posts.forEach(post => {
+  posts.forEach((post, index) => {
     const postElement = document.createElement("div");
     postElement.classList.add("post");
 
@@ -19,8 +19,13 @@ function renderPosts() {
     const postContent = document.createElement("p");
     postContent.textContent = post.content;
 
+    const likeButton = document.createElement("button");
+    likeButton.textContent = `Like (${post.likes})`;
+    likeButton.onclick = () => likePost(index);
+
     postElement.appendChild(postTitle);
     postElement.appendChild(postContent);
+    postElement.appendChild(likeButton);
 
     postContainer.appendChild(postElement);
   });
@@ -35,10 +40,16 @@ function addPost() {
     return;
   }
 
-  const newPost = { title: "New Blog Post", content: postContent };
+  const newPost = { title: "New Blog Post", content: postContent, likes: 0 };
   posts.push(newPost);
   renderPosts();
   document.getElementById("post-content").value = ''; // Clear the textarea
+}
+
+// Function to like a post
+function likePost(index) {
+  posts[index].likes += 1;
+  renderPosts();
 }
 
 // Initial render of posts
